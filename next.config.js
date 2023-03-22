00eec64d175bcf3006d4c787565682ca9a474ed7
fgvlasov <fgvlasov@gmail.com>
2023-03-22 08:30:26 +0300

    Revert "example sample"
    This reverts commit 9da8878acbf232c981d2f11c7fb1684c4328470b.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextTranslate = require("next-translate-plugin");
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-module.exports = nextTranslate({
-	reactStrictMode: true,
-})
+module.exports = nextConfig
--- pages/site/settings/index.js
+++
@@ -1,8 +0,0 @@
-import useTranslation from 'next-translate/useTranslation'
-
-export default function SiteSettingsPage() {
-  //const { t } = useTranslation("common")
-  const { t } = useTranslation()
-
-  return <h1>{t('title')}</h1>
-}
